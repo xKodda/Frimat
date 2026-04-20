0fb6025c919fb11b6ab68b10b802591fc584ba42
christofer <christofergodcer@gmail.com>
2026-04-19 21:33:06 -0400

    se corrige error de type en algunas clases

--- frimat/components/landing/ProjectsSection.tsx
+++ frimat/components/landing/ProjectsSection.tsx
@@ -15,7 +15,7 @@ const container: Variants = {
 
 const item: Variants = {
   hidden: { opacity: 0, y: 30 },
-  show: { opacity: 1, y: 0, transition: { duration: 0.8, ease: "easeOut" } }
+  show: { opacity: 1, y: 0, transition: { duration: 0.8 } }
 };
 
 export function ProjectsSection() {
